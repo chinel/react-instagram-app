10fa8a2864bea59f2357ad3eb391fe8e6f43d077
nelo <nelokwaz@gmail.com>
2021-02-05 21:37:35 -1200

    structured the layout component

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useLayoutStyles } from "../../styles";
 
-function Layout() {
-  useLayoutStyles();
+function Layout({ children }) {
+  const classes = useLayoutStyles();
 
-  return <div>Layout</div>;
+  return (
+    <section className={classes.section}>
+      <main className={classes.main}>
+        <section className={classes.childrenWrapper}>
+          <div className={classes.children}>{children}</div>
+        </section>
+      </main>
+    </section>
+  );
 }
 
 export default Layout;
