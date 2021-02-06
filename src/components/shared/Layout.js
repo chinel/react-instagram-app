1b28917a4d1773f36b606ebed1d6a0e11bf8183b
nelo <nelokwaz@gmail.com>
2021-02-05 21:55:42 -1200

    pulled out the title props from the SEO component

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useLayoutStyles } from "../../styles";
 import SEO from "../shared/Seo";
-function Layout({ children }) {
+
+function Layout({ children, title }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
+      <SEO title={title} />
       <main className={classes.main}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
--- src/components/shared/Seo.js
+++ src/components/shared/Seo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-function SEO() {
+function SEO({ title }) {
+  const titleText = title ? `${title} . Instagram` : "Instagram";
   return (
     <Helmet>
-      <title>title</title>
+      <title>{titleText}</title>
     </Helmet>
   );
 }
