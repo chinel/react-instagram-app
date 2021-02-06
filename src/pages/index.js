a0ece2a5c59cea32c617d2e23aa154e8ff1a99c5
nelo <nelokwaz@gmail.com>
2021-02-05 19:51:53 -1200

    add all the pages to the pages reference file

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as FeedPage } from "./feed";
+export { default as EditProfilePage } from "./edit-profile";
+export { default as ExplorePage } from "./explore";
+export { default as LoginPage } from "./login";
+export { default as NotFoundPage } from "./not-found";
+export { default as PostPage } from "./post";
+export { default as ProfilePage } from "./profile";
+export { default as SignUpPage } from "./signup";
