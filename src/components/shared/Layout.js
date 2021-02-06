28a43284bcdad3f3893ea3ccdb5cac6914200164
nelo <nelokwaz@gmail.com>
2021-02-06 14:53:03 +0100

    made some corrections

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -3,14 +3,14 @@ import { useLayoutStyles } from "../../styles";
 import SEO from "../shared/Seo";
 import Navbar from "./Navbar";
 
-function Layout({ children, title }) {
+function Layout({ children, title, marginTop = 60 }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
       <SEO title={title} />
       <Navbar />
-      <main className={classes.main}>
+      <main className={classes.main} style={{marginTop}}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
         </section>
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,8 +1,24 @@
 import React from "react";
 import Layout from "../components/shared/Layout";
+import { Link, Typography } from "@material-ui/core";
 
 function NotFoundPage() {
-  return <Layout title="Page Not Found">NotFoundPage</Layout>;
+  return (
+    <Layout title="Page Not Found" marginTop={120}>
+      <Typography variant="h5" align="center" paragraph>
+        Sorry, this page isn't available.
+      </Typography>
+      <Typography align="center">
+        The link you followed may be broken, or the page may have been removed.
+        <Link to="/">
+          {" "}
+          <Typography color="primary" component="span">
+            Go back to Instagram.
+          </Typography>
+        </Link>
+      </Typography>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
