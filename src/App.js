82332a595de76e4378b397bfa62d2c9d4f223d26
nelo <nelokwaz@gmail.com>
2021-02-05 19:09:32 -1200

    setup the routing for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FeedPage from "./pages/feed";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={FeedPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
