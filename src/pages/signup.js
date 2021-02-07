3bdafa5269b6aad720f80b596b6b876ef4f2fc01
nelo <nelokwaz@gmail.com>
2021-02-07 11:51:40 +0100

    added the base structure for the signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,9 +2,70 @@ import React from "react";
 import { useSignUpPageStyles } from "../styles";
 
 function SignUpPage() {
-  useSignUpPageStyles();
+  const classes = useSignUpPageStyles();
 
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <SEO title="Login" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <form>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Log In
+              </Button>
+            </form>
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine} />
+            </div>
+            <LoginWithFacebook color="secondary" iconColor="blue" />
+            <Button fullWidth color="secondary">
+              <Typography variant="caption">Forgot password?</Typography>
+            </Button>
+          </Card>
+          <Card className={classes.signUpCard}>
+            <Typography variant="body2" align="right">
+              Don't have an account?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.signUpButton}>
+                Sign up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default SignUpPage;
