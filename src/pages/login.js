95dc57e39d59f7cc530f2dbd4af7f6e807061556
nelo <nelokwaz@gmail.com>
2021-02-07 08:53:40 +0100

    added the structure of the login component

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,80 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import SEO from "../components/shared/Seo";
 import { useLoginPageStyles } from "../styles";
 
 function LoginPage() {
-  useLoginPageStyles();
+  const classes = useLoginPageStyles();
 
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <SEO title="Login" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <form>
+              <TextField
+                fullwidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullwidth
+                variant="filled"
+                label="Password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullwidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Log In
+              </Button>
+            </form>
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine} />
+            </div>
+            <LoginWithFacebook color="secondary" iconColor="blue" />
+            <Button fullWidth color="secondary">
+              <Typography variant="caption">Forgot password?</Typography>
+            </Button>
+          </Card>
+          <Card className={classes.signUpCard}>
+            <Typography variant="body2" align="right">
+              Don't have an account?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.signUpButton}>
+                Sign up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default LoginPage;
