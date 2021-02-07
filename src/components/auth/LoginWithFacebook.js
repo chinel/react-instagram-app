fa23dab4f0af796aebadc2278035457a1f2ec58d
nelo <nelokwaz@gmail.com>
2021-02-07 11:18:39 +0100

    completed the login in with facebook component

--- src/components/auth/LoginWithFacebook.js
+++ src/components/auth/LoginWithFacebook.js
@@ -2,12 +2,21 @@ import React from "react";
 import { useLoginPageStyles } from "../../styles";
 import FacebookIconBlue from "../images/facebook-icon-blue.svg";
 import FacebookIconWhite from "../images/facebook-icon-white.png";
+import { Button } from "@material-ui/core";
 
-function LoginWithFacebook({color, iconColor}) {
-   const classes = useLoginPageStyles();  
-   const facebookIcon = iconColor === 'blue' ? FacebookIconBlue : FacebookIconWhite;
-    return (
-        
-    )
+function LoginWithFacebook({ color, iconColor }) {
+  const classes = useLoginPageStyles();
+  const facebookIcon =
+    iconColor === "blue" ? FacebookIconBlue : FacebookIconWhite;
+  return (
+    <Button fullWidth color={color}>
+      <img
+        src={facebookIcon}
+        alt="Facebook Icon"
+        className={classes.facebookIcon}
+      />
+      Log In with Facebook
+    </Button>
+  );
 }
 export default LoginWithFacebook;
