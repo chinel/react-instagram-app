3db7de5297dc5989291c79651dcde0683794da76
nelo <nelokwaz@gmail.com>
2021-02-07 11:11:19 +0100

    conditionally displayed the facebook icon

--- src/components/auth/LoginWithFacebook.js
+++ src/components/auth/LoginWithFacebook.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useLoginPageStyles } from "../../styles";
+import FacebookIconBlue from "../images/facebook-icon-blue.svg";
+import FacebookIconWhite from "../images/facebook-icon-white.png";
 
-function LoginWithFacebook() {}
-
+function LoginWithFacebook({color, iconColor}) {
+   const classes = useLoginPageStyles();  
+   const facebookIcon = iconColor === 'blue' ? FacebookIconBlue : FacebookIconWhite;
+    return (
+        
+    )
+}
 export default LoginWithFacebook;
