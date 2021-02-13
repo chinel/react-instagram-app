10f3903508aae098ccdb02ee6e1cf5bafed80661
nelo <nelokwaz@gmail.com>
2021-02-12 14:26:01 -1200

    created the skeletal structure for the feeds

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
+import { getDefaultPost } from "../data";
+import FeedPost from "../components/feed/FeedPost";
 
 function FeedPage() {
- const classes =  useFeedPageStyles();
+  const classes = useFeedPageStyles();
 
-  return <Layout><div className={classes.container}>
-    
-    </div></Layout>;
+  return (
+    <Layout>
+      <div className={classes.container}>
+        <div>
+          {Array.from({ length: 5 }, () => getDefaultPost()).map((post) => (
+            <FeedPost key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default FeedPage;
