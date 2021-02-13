3c8be82400901a7ba723ef99fbfd8511dcc29e4a
nelo <nelokwaz@gmail.com>
2021-02-12 14:48:51 -1200

    created the structure of the feed post component

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { MoreIcon } from "../../icons";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const { media } = post;
+  return (
+    <>
+      <article className={classes.article}>
+        {/*Feed Post Header */}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        {/*Feed Post Image */}
+        <div>
+          <img src={media} alt="Post Media" className={classes.image} />
+        </div>
+        {/*Feed Post Buttons*/}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
 
-  return <div>FeedPost</div>;
+          </div>
+        </div>
+      </article>
+    </>
+  );
 }
 
 export default FeedPost;
