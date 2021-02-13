cfc69dc10faaa09ec5b54e3dfffbc91657bd117a
nelo <nelokwaz@gmail.com>
2021-02-13 09:24:25 +0100

    create the structure of the like button component

--- src/components/feed/LikeButton.js
+++ src/components/feed/LikeButton.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { LikeIcon, UnlikeIcon } from "../../icons";
+import { useFeedPostStyles } from "../../styles";
 
 function LikeButton() {
-  return <div></div>;
+  const classes = useFeedPostStyles();
+  const [liked, setLiked] = React.useState(false);
+  const Icon = liked ? UnlikeIcon : LikeIcon;
+  const className = liked ? classes.liked : classes.like;
+  const onClick = liked ? handleUnlike : handleLike;
+
+  function handleLike(){
+
+  }
+
+  function handleUnlike(){
+      
+  }
+
+   
+  return <Icon className={className} onClick={onClick}/>;
 }
 
 export default LikeButton;
