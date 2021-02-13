3773d67977f232bd6b8d84d91d787d9a32c8495f
nelo <nelokwaz@gmail.com>
2021-02-12 14:36:45 -1200

    created the skeletal structure of the feedpage's sidebar

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
+import UserCard from "../components/shared/UserCard";
 import { getDefaultPost } from "../data";
 import FeedPost from "../components/feed/FeedPost";
+import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
+import { Hidden } from "@material-ui/core";
 
 function FeedPage() {
   const classes = useFeedPageStyles();
@@ -10,11 +13,21 @@ function FeedPage() {
   return (
     <Layout>
       <div className={classes.container}>
+        {/*Feed Posts */}
         <div>
           {Array.from({ length: 5 }, () => getDefaultPost()).map((post) => (
             <FeedPost key={post.id} post={post} />
           ))}
         </div>
+        {/*Sidebar */}
+        <Hidden smDown>
+          <div className={classes.sidebarContainer}>
+            <div className={classes.sidebarWrapper}>
+              <UserCard />
+              <FeedSideSuggestions />
+            </div>
+          </div>
+        </Hidden>
       </div>
     </Layout>
   );
