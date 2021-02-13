f01c0e164d805c86402949705fb5bc7485438b47
nelo <nelokwaz@gmail.com>
2021-02-12 15:28:15 -1200

    added the feed post captions

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,13 +1,17 @@
+import { Button, Typography } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
 import { CommentIcon, MoreIcon, ShareIcon } from "../../icons";
 import { useFeedPostStyles } from "../../styles";
 import UserCard from "../shared/UserCard";
 import LikeButton from "./LikeButton";
+import SaveButton from "./SaveButton";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
 function FeedPost({ post }) {
   const classes = useFeedPostStyles();
-  const { id, media } = post;
+  const [showCaption, setShowCaption] = React.useState(false);
+  const { id, media, likes, user, caption } = post;
   return (
     <>
       <article className={classes.article}>
@@ -28,6 +32,44 @@ function FeedPost({ post }) {
               <CommentIcon />
             </Link>
             <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {showCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              />
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafehtml={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setShowCaption(true)}
+                >
+                  more
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </article>
