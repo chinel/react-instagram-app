db21feb80124d4eaaf01132b208066bdbd937ac1
nelo <nelokwaz@gmail.com>
2021-02-14 12:16:28 +0100

    created the structure of the loading component

--- src/components/shared/LoadingScreen.js
+++ src/components/shared/LoadingScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { LoadingIcon } from "../../icons";
 import { useLoadingScreenStyles } from "../../styles";
 
 function LoadingScreen() {
-  useLoadingScreenStyles();
+  const classes = useLoadingScreenStyles();
 
-  return <div>LoadingScreen</div>;
+  return (
+    <section className={classes.section}>
+      <span>
+        <LoadingIcon />
+      </span>
+    </section>
+  );
 }
 
 export default LoadingScreen;
