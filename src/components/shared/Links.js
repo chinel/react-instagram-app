38479f1f281c70ada0631620ccdee52d994c8007
nelo <nelokwaz@gmail.com>
2021-02-15 19:57:56 -1200

    added the layout strucuture of the nav links component

--- src/components/shared/Links.js
+++ src/components/shared/Links.js
@@ -8,11 +8,43 @@ import {
   ExploreActiveIcon,
   HomeIcon,
   HomeActiveIcon,
- } from "../../icons";
+} from "../../icons";
+import { Avatar, Hidden } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { defaultCurrentUser } from "../../data";
 
-function Links() {
+function Links({ path }) {
   const classes = useNavbarStyles();
-  return;
+  const [showList, setShowList] = React.useState(false);
+  return (
+    <div className={classes.linksContainer}>
+      <div className={classes.linksWrapper}>
+        <Hidden xsDown>
+          <AddIcon />
+        </Hidden>
+        <Link to="/">{path === "/" ? <HomeActiveIcon /> : <HomeIcon />}</Link>
+        <Link to="/explore">
+          {path === "/explore" ? <ExploreActiveIcon /> : <ExploreIcon />}
+        </Link>
+        <div className={classes.notifications}>
+          {showList ? <LikeActiveIcon /> : <LikeIcon />}
+        </div>
+        <Link to={`/${defaultCurrentUser.username}`}>
+          <div
+            className={
+              path === `/${defaultCurrentUser.username}`
+                ? classes.profileActive
+                : ""
+            }
+          ></div>
+          <Avatar
+            src={defaultCurrentUser.profile_image}
+            className={classes.profileImage}
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Links;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -4,9 +4,12 @@ import { AppBar } from "@material-ui/core";
 import Logo from "./Logo";
 import Search from "./Search";
 import Links from "./Links";
+import { useHistory } from "react-router-dom";
 
 function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
+  const history = useHistory();
+  const path = history.location.pathname;
 
   return (
     <AppBar className={classes.appBar}>
@@ -15,7 +18,7 @@ function Navbar({ minimalNavbar }) {
         {!minimalNavbar && (
           <>
             <Search />
-            <Links />
+            <Links path={path} />
           </>
         )}
       </section>
