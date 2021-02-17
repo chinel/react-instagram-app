3fd64c20a2dece0f2ff641029e7bd8c6c1200b5d
nelo <nelokwaz@gmail.com>
2021-02-17 16:05:14 +0100

    completed the progress bar component

--- src/components/shared/Progress.js
+++ src/components/shared/Progress.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useNavbarStyles } from "../../styles";
+import { useNProgress } from "@tanem/react-nprogress";
+function Progress({ isAnimating }) {
+  const classes = useNavbarStyles();
+  const { animationDuration, isFinished, progress } = useNProgress({
+    isAnimating,
+  });
 
-function Progress() {}
+  return (
+    <div
+      className={classes.progressContainer}
+      style={{
+        opacity: isFinished ? 0 : 1,
+        transition: `opacity ${animationDuration}ms linear`,
+      }}
+    >
+      <div
+        className={classes.progress}
+        style={{
+          marginLeft: `${(-1 + progress) * 100}%`,
+          transition: `margin-left ${animationDuration}ms linear`,
+        }}
+      >
+        <div className={classes.progressBackground} />
+      </div>
+    </div>
+  );
+}
 
 export default Progress;
