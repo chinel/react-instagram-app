655359527d7de3c915a893a1ca298a1ffd0c7688
nelo <nelokwaz@gmail.com>
2021-02-17 13:14:20 +0100

    completed the notification list dropdown

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -1,10 +1,57 @@
+import { Avatar, Grid, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import { defaultNotifications } from "../../data";
 import { useNotificationListStyles } from "../../styles";
+import FollowButton from "../shared/FollowButton";
 
 function NotificationList() {
-  useNotificationListStyles();
+  const classes = useNotificationListStyles();
 
-  return <div>NotificationList</div>;
+  return (
+    <Grid className={classes.listContainer}>
+      {defaultNotifications.map((notification) => {
+        const isLike = notification.type === "like";
+        const isFollow = notification.type === "follow";
+
+        return (
+          <Grid key={notification.id} item className={classes.listItem}>
+            <div className={classes.listItemWrapper}>
+              <div className={classes.avatarWrapper}>
+                <Avatar
+                  src={notification.user.profile_image}
+                  alt="User avatar"
+                />
+              </div>
+              <div className={classes.nameWrapper}>
+                <Link to={`/${notification.user.username}`}>
+                  <Typography variant="body1">
+                    {notification.user.username}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    className={classes.typography}
+                  >
+                    {isLike && "likes your photo. 4d"}
+                    {isFollow && `started following you. 5d`}
+                  </Typography>
+                </Link>
+              </div>
+            </div>
+            <div>
+              {isLike && (
+                <Link to={`/p/${notification.post.id}`}>
+                  <Avatar src={notification.post.media} alt="post cover" />
+                </Link>
+              )}
+              {isFollow && <FollowButton />}
+            </div>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 
 export default NotificationList;
