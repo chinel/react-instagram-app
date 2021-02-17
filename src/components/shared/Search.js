d18e9f7790e806b5cdda137fdb75b4b61317eaef
nelo <nelokwaz@gmail.com>
2021-02-16 22:26:02 -1200

    created the dropdown tooltip for the search nav menu

--- src/components/shared/Search.js
+++ src/components/shared/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { useNavbarStyles } from "../../styles";
-import { Hidden, InputBase } from "@material-ui/core";
+import { useNavbarStyles, WhiteTooltip } from "../../styles";
+import { Avatar, Fade, Grid, Hidden, InputBase, Typography } from "@material-ui/core";
 import { LoadingIcon } from "../../icons";
 import { getDefaultUser } from "../../data";
 
@@ -22,20 +22,50 @@ function Search() {
   };
   return (
     <Hidden xsDown>
-      <InputBase
-        className={classes.input}
-        onChange={(event) => setQuery(event.target.value)}
-        startAdornment={<span className={classes.searchIcon} />}
-        endAdornment={
-          loading ? (
-            <LoadingIcon />
-          ) : (
-            <span onClick={handleClearInput} className={classes.clearIcon} />
+      <WhiteTooltip
+        arrow
+        interactive
+        TransitionComponent={Fade}
+        open={hasResults}
+        title={
+          hasResults && (
+            <Grid className={classes.resultContainer} container>
+              {results.map((result) => (
+                <Grid key={result.id} item className={classes.resultLink}>
+                  <div className={classes.resultWrapper}>
+                    <div className={classes.avatarWrapper}>
+                      <Avatar src={result.profile_image} alt="user avatar" />
+                    </div>
+                    <div className={classes.nameWrapper}>
+                      <Typography variant="body1">
+                        {result.username}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                         {result.name}
+                      </Typography>
+                    </div>
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
           )
         }
-        placeholder="Search"
-        value={query}
-      />
+      >
+        <InputBase
+          className={classes.input}
+          onChange={(event) => setQuery(event.target.value)}
+          startAdornment={<span className={classes.searchIcon} />}
+          endAdornment={
+            loading ? (
+              <LoadingIcon />
+            ) : (
+              <span onClick={handleClearInput} className={classes.clearIcon} />
+            )
+          }
+          placeholder="Search"
+          value={query}
+        />
+      </WhiteTooltip>
     </Hidden>
   );
 }
