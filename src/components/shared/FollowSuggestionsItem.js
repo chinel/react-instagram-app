cd03b8c1846443674308ae423d70ba4eb73685f0
nelo <nelokwaz@gmail.com>
2021-02-18 21:08:13 -1200

    completed the individual slide item compoennt

--- src/components/shared/FollowSuggestionsItem.js
+++ src/components/shared/FollowSuggestionsItem.js
@@ -1,3 +1,45 @@
+import { Avatar, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import { useFollowSuggestionsStyles } from "../../styles";
+import FollowButton from "./FollowButton";
 
-function FollowSuggestionsItem() {}
+function FollowSuggestionsItem({ user }) {
+  const classes = useFollowSuggestionsStyles();
+  const { profile_image, username, name } = user;
+
+  return (
+    <div>
+      <div className={classes.card}>
+        <Link to={`/${username}`}>
+          <Avatar
+            src={profile_image}
+            alt={`${username}'s profile`}
+            classes={{
+              root: classes.avatar,
+              img: classes.avatarImg,
+            }}
+          />
+        </Link>
+        <Link to={`/${username}`}>
+          <Typography
+            variant="subtitle2"
+            className={classes.text}
+            align="center"
+          >
+            {username}
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="body2"
+            className={classes.text}
+            align="center"
+          >
+            {name}
+          </Typography>
+          <FollowButton side={false} />
+        </Link>
+      </div>
+    </div>
+  );
+}
