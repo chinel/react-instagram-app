ac4db0f349239f065da80681417d9a9a734029cd
nelo <nelokwaz@gmail.com>
2021-02-20 20:10:00 +0100

    added the base structure for the post component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,10 +1,128 @@
+import { Button, Divider, Hidden, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import { CommentIcon, MoreIcon, ShareIcon } from "../../icons";
 import { usePostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import LikeButton from "./LikeButton";
+import SaveButton from "./SaveButton";
+import Comment from "./Comment";
+import FollowSuggestions from "../shared/FollowSuggestions";
+import OptionsDialog from "../shared/OptionsDialog";
+import { defaultPost } from "../../data";
 
 function Post() {
-  usePostStyles();
+  const classes = usePostStyles();
+  const [showOptionsDialog, setShowOptionsDialog] = React.useState(false);
+  const { id, media, likes, user, caption, comments } = defaultPost;
+  return (
+    <>
+      <article
+        className={classes.article}
+        style={{ marginBottom: showFollowSuggestion && 30 }}
+      >
+        {/*Feed Post Header */}
+        <div className={classes.postHeader}>
+          {user && <UserCard user={user} />}
+          <MoreIcon
+            className={classes.moreIcon}
+            onClick={() => setShowOptionsDialog(true)}
+          />
+        </div>
+        {/*Feed Post Image */}
+        <div>
+          <img src={media} alt="Post Media" className={classes.image} />
+        </div>
+        {/*Feed Post Buttons*/}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.like} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
 
-  return <div>Post</div>;
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {showCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              />
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafeHTML={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setShowCaption(true)}
+                >
+                  more
+                </Button>
+              </div>
+            )}
+          </div>
+
+          <Link to={`/p/${id}`}>
+            <Typography
+              className={classes.commentsLink}
+              variant="body2"
+              component="div"
+            >
+              View all {comments.length} comments
+            </Typography>
+          </Link>
+
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <Link to={`/${comment.user.username}`}>
+                <Typography
+                  variant="subtitle2"
+                  component="span"
+                  className={classes.commentUsername}
+                >
+                  {comment.user.username}
+                </Typography>{" "}
+                <Typography variant="body2" component="span">
+                  {comment.content}
+                </Typography>
+              </Link>
+            </div>
+          ))}
+          <Typography color="textSecondary" className={classes.datePosted}>
+            5 DAYS AGO
+          </Typography>
+        </div>
+        <Hidden xsDown>
+          <Divider />
+          <Comment />
+        </Hidden>
+      </article>
+      {showFollowSuggestion && <FollowSuggestions />}
+      {showOptionsDialog && (
+        <OptionsDialog onClose={() => setShowOptionsDialog(false)} />
+      )}
+    </>
+  );
 }
 
 export default Post;
