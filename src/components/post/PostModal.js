dda1302b4b5853db9dd67568b45b40bd9d3a6bfc
nelo <nelokwaz@gmail.com>
2021-02-20 15:41:09 +0100

    added the post modal structure

--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,10 +1,38 @@
 import React from "react";
+import Modal from "react-modal";
+import { useHistory } from "react-router-dom";
 import { usePostModalStyles } from "../../styles";
 
 function PostModal() {
-  usePostModalStyles();
+  const history = useHistory();
+  const classes = usePostModalStyles();
 
-  return <div>PostModal</div>;
+  return (
+    <>
+      <Modal
+        isOpen
+        overlayClassName={classes.overlay}
+        onRequestClose={() => history.goBack()}
+        style={{
+          content: {
+            display: "flex",
+            alignItems: "center",
+            maxWidth: 935,
+            width: "100%",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            margin: 0,
+            padding: 0,
+            overflow: "none",
+            WebkitOverflowScrolling: "touch",
+          },
+        }}
+      ></Modal>
+    </>
+  );
 }
 
 export default PostModal;
