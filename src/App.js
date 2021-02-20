9e9d6e53e5c1ce6421361d7bcf1516a10f82f770
nelo <nelokwaz@gmail.com>
2021-02-19 22:23:44 -1200

    moved the Browser Router up one level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import {
   EditProfilePage,
   ExplorePage,
@@ -10,21 +10,20 @@ import {
   ProfilePage,
   SignUpPage,
 } from "./pages";
+import PostModal from "./components/post/PostModal";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={FeedPage} />
-        <Route path="/explore" component={ExplorePage} />
-        <Route exact path="/:username" component={ProfilePage} />
-        <Route exact path="/p/:postId" component={PostPage} />
-        <Route path="/accounts/edit" component={EditProfilePage} />
-        <Route path="/accounts/login" component={LoginPage} />
-        <Route path="/accounts/emailsignup" component={SignUpPage} />
-        <Route path="*" component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={FeedPage} />
+      <Route path="/explore" component={ExplorePage} />
+      <Route exact path="/:username" component={ProfilePage} />
+      <Route exact path="/p/:postId" component={PostPage} />
+      <Route path="/accounts/edit" component={EditProfilePage} />
+      <Route path="/accounts/login" component={LoginPage} />
+      <Route path="/accounts/emailsignup" component={SignUpPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router } from "react-router-dom";
 import theme from "./theme";
 import App from "./App";
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </MuiThemeProvider>,
   document.getElementById("root")
 );
