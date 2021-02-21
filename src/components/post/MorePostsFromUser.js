8356c1efc43614d71ac444fd5fae77a3cbe21e27
nelo <nelokwaz@gmail.com>
2021-02-21 08:26:37 +0100

    added the base structure of the more post from user component

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,10 +1,41 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import { getDefaultPost } from "../../data";
+import { LoadingLargeIcon } from "../../icons";
 import { useMorePostsFromUserStyles } from "../../styles";
+import GridPost from "../shared/GridPost";
 
 function MorePostsFromUser() {
-  useMorePostsFromUserStyles();
+  const classes = useMorePostsFromUserStyles();
 
-  return <div>MorePostsFromUser</div>;
+  let loading = false;
+
+  return (
+    <>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        component="h2"
+        gutterBottom
+        className={classes.typography}
+      >
+        Explore
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 20 }, () => getDefaultPost()).map(
+              (post, index) => (
+                <GridPost key={index} post={post} />
+              )
+            )}
+          </div>
+        </article>
+      )}
+    </>
+  );
 }
 
-export default MorePostsFromUser;
+export default ExploreGrid;
