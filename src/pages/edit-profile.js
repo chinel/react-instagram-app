b166e74b7c61723877a2f5fea13b3cb9a0b00bd1
nelo <nelokwaz@gmail.com>
2021-02-22 18:59:37 -1200

    created the basic structure of the edit profile page

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,41 @@
 import React from "react";
+import Layout from "../components/shared/Layout";
 import { useEditProfilePageStyles } from "../styles";
+import { Drawer, Hidden, IconButton } from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 
 function EditProfilePage() {
-  useEditProfilePageStyles();
+  const classes = useEditProfilePageStyles();
+  const [showDrawer, setShowDrawer] = React.useState(false);
 
-  return <div>EditProfilePage</div>;
+  function handleToggleDrawer() {
+    setShowDrawer((prev) => !prev);
+  }
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton
+          edge="start"
+          onClick={handleToggleDrawer}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            ></Drawer>
+          </Hidden>
+        </nav>
+      </section>
+    </Layout>
+  );
 }
 
 export default EditProfilePage;
