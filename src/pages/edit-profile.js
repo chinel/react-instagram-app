4309641a74c35a8103d2126d21ecff5b86f3476b
nelo <nelokwaz@gmail.com>
2021-02-22 19:42:57 -1200

    created the function to handle the selected menu item

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Layout from "../components/shared/Layout";
 import { useEditProfilePageStyles } from "../styles";
-import { Drawer, Hidden, IconButton, List } from "@material-ui/core";
+import { Drawer, Hidden, IconButton, List, ListItem } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
 
-function EditProfilePage() {
+function EditProfilePage({ history }) {
   const classes = useEditProfilePageStyles();
   const [showDrawer, setShowDrawer] = React.useState(false);
 
   function handleToggleDrawer() {
     setShowDrawer((prev) => !prev);
+  }
+
+  function handleSelected(index) {
+    const path = history.location.pathname;
+    switch (index) {
+      case 0:
+        return path.includes("edit");
+      default:
+        break;
+    }
   }
 
   const options = [
@@ -24,7 +35,17 @@ function EditProfilePage() {
     "Emails from Instagram",
   ];
 
-  const drawer = <List></List>;
+  const drawer = (
+    <List>
+      {options.map((option, index) => (
+        <ListItem
+          key={option}
+          button
+          selected={handleSelected(index)}
+        ></ListItem>
+      ))}
+    </List>
+  );
 
   return (
     <Layout title="Edit Profile">
