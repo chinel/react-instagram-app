6aaa833801974c1971cb6dd4d2e15ba624c6accc
nelo <nelokwaz@gmail.com>
2021-02-28 19:42:53 -1200

    added the base structure of the profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,30 @@
+import { Card, CardContent, Hidden } from "@material-ui/core";
 import React from "react";
 import Layout from "../components/shared/Layout";
+import ProfilePicture from "../components/shared/ProfilePicture";
+import { defaultCurrentUser } from "../data";
 import { useProfilePageStyles } from "../styles";
 
 function ProfilePage() {
-  useProfilePageStyles();
+  const classes = useProfilePageStyles();
 
-  return <Layout>ProfilePage</Layout>;
+  return (
+    <Layout
+      title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`}
+    >
+      <div className={classes.container}>
+        <Hidden xsDown>
+          <Card className={classes.cardLarge}>
+            <ProfilePicture />
+            <CardContent className={classes.cardContentLarge}>
+              
+            </CardContent>
+          </Card>
+        </Hidden>
+        <Hidden smUp></Hidden>
+      </div>
+    </Layout>
+  );
 }
 
 export default ProfilePage;
