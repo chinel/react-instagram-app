08ec9558b7ae65eac64801d9288348eb955ef0e8
nelo <nelokwaz@gmail.com>
2021-03-03 11:47:01 -1200

    added the functionality to handle logging out

--- src/components/profile/OptionsMenu.js
+++ src/components/profile/OptionsMenu.js
@@ -1,9 +1,20 @@
-import { Button, Dialog, Zoom } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Typography,
+  Zoom,
+} from "@material-ui/core";
 import React from "react";
 import { useProfilePageStyles } from "../../styles";
 
 function OptionsMenu() {
   const classes = useProfilePageStyles();
+  const [showLogoutMessage, setShowLogoutMessage] = React.useState(false);
+
+  function handleLogoutClick() {
+    setShowLogoutMessage(true);
+  }
 
   return (
     <Dialog
@@ -14,13 +25,24 @@ function OptionsMenu() {
       }}
       TransitionComponent={Zoom}
     >
-      <OptionsItem text="Change Password" />
-      <OptionsItem text="Nametag" />
-      <OptionsItem text="Authorized Apps" />
-      <OptionsItem text="Notifications" />
-      <OptionsItem text="Privacy and Security" />
-      <OptionsItem text="Log Out" />
-      <OptionsItem text="Change Password" />
+      {showLogoutMessage ? (
+        <DialogTitle className={classes.dialogTitle}>
+          Logging Out
+          <Typography color="textSecondary">
+            You need to log back in to continue using Instagram
+          </Typography>
+        </DialogTitle>
+      ) : (
+        <>
+          <OptionsItem text="Change Password" />
+          <OptionsItem text="Nametag" />
+          <OptionsItem text="Authorized Apps" />
+          <OptionsItem text="Notifications" />
+          <OptionsItem text="Privacy and Security" />
+          <OptionsItem text="Log Out" onClick={handleLogoutClick} />
+          <OptionsItem text="Change Password" />
+        </>
+      )}
     </Dialog>
   );
 }
