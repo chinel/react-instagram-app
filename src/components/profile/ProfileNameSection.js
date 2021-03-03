0f5fc51a4c44fcddfcecbb6e96a1854199101ff5
nelo <nelokwaz@gmail.com>
2021-03-02 21:48:39 -1200

    created the layout structure of the profile header section for larger screen view

--- src/components/profile/ProfileNameSection.js
+++ src/components/profile/ProfileNameSection.js
@@ -1,7 +1,59 @@
+import { Button, Hidden, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import { GearIcon } from "../../icons";
+import { useProfilePageStyles } from "../../styles";
 
-function ProfileNameSection() {
-  return <div>Profile Name Section</div>;
+function ProfileNameSection({ user, isOwner, handleOptionsMenuClick }) {
+  const classes = useProfilePageStyles();
+  let followButton;
+  const isFollowing = false;
+  const isFollower = false;
+  if (isFollowing) {
+    followButton = (
+      <Button variant="outlined" className={classes.button}>
+        Following
+      </Button>
+    );
+  } else if (isFollower) {
+    followButton = (
+      <Button variant="contained" color="primary" className={classes.button}>
+        Follow Back
+      </Button>
+    );
+  } else {
+    followButton = (
+      <Button variant="contained" color="primary" className={classes.button}>
+        Follow
+      </Button>
+    );
+  }
+
+  return (
+    <>
+      <Hidden xsDown>
+        <section className={classes.usernameSection}>
+          <Typography className={classes.username}>{user.username}</Typography>
+        </section>
+        {isOwner ? (
+          <>
+            <Link to="/account/edit">
+              <Button variant="outlined">Edit Profile</Button>
+            </Link>
+            <div
+              onClick={handleOptionsMenuClick}
+              className={classes.settingsWrapper}
+            >
+              <GearIcon className={classes.settings} />
+            </div>
+          </>
+        ) : (
+          <>{followButton}</>
+        )}
+      </Hidden>
+      <Hidden smUp></Hidden>
+    </>
+  );
 }
 
 export default ProfileNameSection;
