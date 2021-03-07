5e3d619c5d8d095f4f5e36c9869a988287e5053b
nelo <nelokwaz@gmail.com>
2021-03-06 22:26:28 -1200

    created the profile tab navigation for the large screens

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -1,10 +1,50 @@
+import { Divider, Hidden, Tab, Tabs } from "@material-ui/core";
 import React from "react";
 import { useProfileTabsStyles } from "../../styles";
 
-function ProfileTabs() {
-  useProfileTabsStyles();
+function ProfileTabs({ user, isOwner }) {
+  const classes = useProfileTabsStyles();
+  const [value, setValue] = React.useState(0);
 
-  return <div>ProfileTabs</div>;
+  return (
+    <>
+      <section className={classes.section}>
+        <Hidden xsDown>
+          <Divider />
+        </Hidden>
+        <Hidden xsDown>
+          <Tabs
+            value={value}
+            onChange={(_, value) => setValue(value)}
+            centered
+            classes={{ indicator: classes.tabsIndicator }}
+          >
+            <Tab
+              icon={<span className={classes.postIconLarge} />}
+              label="POSTS"
+              classes={{
+                root: classes.tabRoot,
+                labelIcon: classes.tabLabelIcon,
+                wrapper: classes.tabWrapper,
+              }}
+            />
+
+            {isOwner && (
+              <Tab
+                icon={<span className={classes.savedIconLarge} />}
+                label="SAVED"
+                classes={{
+                  root: classes.tabRoot,
+                  labelIcon: classes.tabLabelIcon,
+                  wrapper: classes.tabWrapper,
+                }}
+              />
+            )}
+          </Tabs>
+        </Hidden>
+      </section>
+    </>
+  );
 }
 
 export default ProfileTabs;
