cf99374caf6636cace76d55d1c0abb61f7d99f83
nelo <nelokwaz@gmail.com>
2021-03-09 10:48:22 +0100

    completed the layout structure of the profile posts component

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -74,8 +74,8 @@ function ProfileTabs({ user, isOwner }) {
           </Tabs>
         </Hidden>
         <Hidden smUp>{user.posts.length === 0 && <Divider />}</Hidden>
-        {value === 0 && <ProfilePosts />}
-        {value === 1 && <SavedPosts />}
+        {value === 0 && <ProfilePosts user={user} isOwner={isOwner} />}
+        {value === 1 && <SavedPosts user={user} />}
       </section>
     </>
   );
