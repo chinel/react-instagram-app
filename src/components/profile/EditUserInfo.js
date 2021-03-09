1119e0703db7693c7157fc9119cb59badd1c70da
nelo <nelokwaz@gmail.com>
2021-03-09 12:59:01 +0100

    removed irrelevant props

--- src/components/profile/EditUserInfo.js
+++ src/components/profile/EditUserInfo.js
@@ -8,7 +8,7 @@ function EditUserInfo({ user }) {
   return (
     <section className={classes.container}>
       <div className={classes.pictureSectionItem}>
-        <ProfilePicture size={38} user={user} />
+        <ProfilePicture size={38} />
         <div className={classes.justifySelfStart}>
           <Typography className={classes.typography}>
             {user.username}
