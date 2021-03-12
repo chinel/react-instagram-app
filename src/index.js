a51519e514e69c5927ced9754f03cbc417647e3e
nelo <nelokwaz@gmail.com>
2021-03-12 10:10:18 +0100

    wrapped the apollo provider around the entire app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import theme from "./theme";
 import App from "./App";
+import client from "../graphql/client";
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <Router>
-      <App />
-    </Router>
-  </MuiThemeProvider>,
+  <ApolloProvider client={client}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+    </MuiThemeProvider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
