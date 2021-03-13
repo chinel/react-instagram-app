08523eb4c4293fc069b4cc73359f3e0c25961a94
nelo <nelokwaz@gmail.com>
2021-03-13 23:43:44 +0100

    added the mutation to create a new user

---
+++ src/graphql/mutations.js
@@ -0,0 +1,29 @@
+import { gql } from "apollo-boost";
+
+export const CREATE_USER = gql`
+  mutation createUsers(
+    $userId: String!
+    $name: String!
+    $username: String!
+    $email: String!
+    $bio: String!
+    $website: String!
+    $profileImage: String!
+    $phoneNumber: String!
+  ) {
+    insert_users(
+      objects: {
+        bio: $bio
+        email: $email
+        name: $name
+        phone_number: $phoneNumber
+        profile_image: $profileImage
+        user_id: $userId
+        username: $username
+        website: $website
+      }
+    ) {
+      affected_rows
+    }
+  }
+`;
