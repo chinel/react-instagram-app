ec1333f023b9d5ae4a16e966d54e19870f627aac
nelo <nelokwaz@gmail.com>
2021-03-15 14:41:27 +0100

    added the layout structure of the auth error component

---
+++ src/components/auth/AuthError.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+function AuthError({ error }) {
+  return (
+    Boolean(error) && (
+      <Typography
+        align="center"
+        gutterBottom
+        variant="body2"
+        style={{ color: "red" }}
+      >
+        {error}
+      </Typography>
+    )
+  );
+}
+
+export default AuthError;
