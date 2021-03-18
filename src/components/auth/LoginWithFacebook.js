5b741199103e1c61979220f9ad6e06fa24faae1a
nelo <nelokwaz@gmail.com>
2021-03-18 08:39:36 +0100

    added onclick event to the login with facebook button

--- src/components/auth/LoginWithFacebook.js
+++ src/components/auth/LoginWithFacebook.js
@@ -3,13 +3,21 @@ import { useLoginPageStyles } from "../../styles";
 import FacebookIconBlue from "../../images/facebook-icon-blue.svg";
 import FacebookIconWhite from "../../images/facebook-icon-white.png";
 import { Button } from "@material-ui/core";
+import { AuthContext } from "../../auth";
+import { useContext } from "react";
 
 function LoginWithFacebook({ color, iconColor, variant }) {
   const classes = useLoginPageStyles();
+  const { loginWithGoogle } = useContext(AuthContext);
   const facebookIcon =
     iconColor === "blue" ? FacebookIconBlue : FacebookIconWhite;
   return (
-    <Button fullWidth color={color} variant={variant}>
+    <Button
+      onClick={handleLogInWithGoogle}
+      fullWidth
+      color={color}
+      variant={variant}
+    >
       <img
         src={facebookIcon}
         alt="Facebook Icon"
