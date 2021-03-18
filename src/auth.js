762f8b445bfe4ab62f9e6ab3697c8acf822bae66
nelo <nelokwaz@gmail.com>
2021-03-17 17:34:25 -1200

    added the loginwithemailandpassword to the auth provider

--- src/auth.js
+++ src/auth.js
@@ -102,6 +102,7 @@ function AuthProvider({ children }) {
           signInWithGoogle,
           signOut,
           signUpWithEmailAndPassword,
+          loginWithEmailAndPassword,
         }}
       >
         {children}
