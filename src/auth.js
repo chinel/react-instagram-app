0514d077f55059f852c2eb26f80077d74781b900
nelo <nelokwaz@gmail.com>
2021-03-22 17:27:46 -1200

    created a function to update the user's email

--- src/auth.js
+++ src/auth.js
@@ -108,6 +108,11 @@ function AuthProvider({ children }) {
     setAuthState({ status: "out" });
   }
 
+  async function updateEmail(email) {
+    await authState.user.updateEmail(email);
+    console.log(authState.user);
+  }
+
   if (authState.status === "loading") {
     return null;
   } else {
