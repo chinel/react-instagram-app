9987526455e1ff9f15d17e4511464d30620949fb
nelo <nelokwaz@gmail.com>
2021-03-24 21:25:45 +0100

    created the logic to get the url of the image uploaded to cloudinary

--- src/utils/handleImageUpload.js
+++ src/utils/handleImageUpload.js
@@ -1,3 +1,21 @@
-function handleImageUpload(image) {}
+//image here is the file data
+async function handleImageUpload(image) {
+  // the file data is going to be collected in the form data constructor
+  const data = new FormData();
+  data.append("file", image);
+  data.append("upload_preset", "instagram"); //this upload preset is from cloudinary;s upload tab under settings
+  data.append("cloud_name", "dzrzvld6q"); // cloudinary name
+  const response = await fetch(
+    "https://api.cloudinary.com/v1_1/dzrzvld6q/image/upload",
+    {
+      method: "POST",
+      accept: "application/json ",
+      body: data,
+    }
+  );
+
+  const jsonResponse = await response.json();
+  return jsonResponse.url;
+}
 
 export default handleImageUpload;
