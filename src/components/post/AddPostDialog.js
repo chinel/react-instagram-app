a42ca4a5f04d3868a7a49f12693ba4b3d4261302
nelo <nelokwaz@gmail.com>
2021-03-27 18:18:27 +0100

    created a slate editor object

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -3,6 +3,8 @@ import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
 function AddPostDialog({ media, handleClose }) {
+  const editor = React.useMemo(() => withReact(createEditor()), []);
+
   return <span>addpost dialog</span>;
 }
 
