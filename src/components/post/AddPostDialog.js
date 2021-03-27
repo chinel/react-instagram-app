c703f2a5bb27ea61770f65d86bb6fbf974346e9e
nelo <nelokwaz@gmail.com>
2021-03-27 18:17:38 +0100

    added the required imports

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
 function AddPostDialog({ media, handleClose }) {
   return <span>addpost dialog</span>;
