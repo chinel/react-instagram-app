3adb3d2a8b994e23780fed1dd2761a4a7609ade3
nelo <nelokwaz@gmail.com>
2021-03-30 18:54:00 +0100

    created a state variable to keep track of the submit status

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -29,10 +29,9 @@ function AddPostDialog({ media, handleClose }) {
   const [value, setValue] = React.useState(initialValue);
   const { me } = React.useContext(UserContext);
   const [location, setLocation] = React.useState("");
-  
-  function handleSharePost(){
-    
-  }
+  const [submitting, setSubmitting] = React.useState(false);
+
+  function handleSharePost() {}
 
   return (
     <Dialog fullScreen open onClose={handleClose}>
