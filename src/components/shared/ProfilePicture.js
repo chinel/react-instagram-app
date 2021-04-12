96f90704b916aa846ccf7bae286e6e1630622581
nelo <nelokwaz@gmail.com>
2021-04-12 07:53:10 +0100

    added an on click logic to the profile picture component

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -5,10 +5,17 @@ import { useProfilePictureStyles } from "../../styles";
 function ProfilePicture({ size, image, isOwner }) {
   const classes = useProfilePictureStyles({ size, isOwner });
 
+  function openFileInput (){
+
+  }
+  
   return (
     <section className={classes.section}>
       {image ? (
-        <div className={classes.wrapper}>
+        <div
+          className={classes.wrapper}
+          onClick={isOwner ? openFileInput : () => null}
+        >
           <img src={image} alt="user profile" className={classes.image} />
         </div>
       ) : (
