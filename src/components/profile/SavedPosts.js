34d0b34633f42798ba58b2471160334269bb4ad0
nelo <nelokwaz@gmail.com>
2021-04-15 20:08:50 +0100

    conditionally rendered the no saved post message if user has no saved post

--- src/components/profile/SavedPosts.js
+++ src/components/profile/SavedPosts.js
@@ -4,18 +4,20 @@ import { useProfileTabsStyles } from "../../styles";
 
 function SavedPosts({ user }) {
   const classes = useProfileTabsStyles();
-  return (
-    <section className={classes.savedPostsSection}>
-      <div className={classes.noContent}>
-        <div className={classes.savePhotoIcon} />
-        <Typography variant="h4">Save</Typography>
-        <Typography align="center">
-          Save photos and videos that you want to see again. No one is notified,
-          and only you can see what you've saved.
-        </Typography>
-      </div>
-    </section>
-  );
+  if (user.saved_posts.length === 0) {
+    return (
+      <section className={classes.savedPostsSection}>
+        <div className={classes.noContent}>
+          <div className={classes.savePhotoIcon} />
+          <Typography variant="h4">Save</Typography>
+          <Typography align="center">
+            Save photos and videos that you want to see again. No one is
+            notified, and only you can see what you've saved.
+          </Typography>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default SavedPosts;
