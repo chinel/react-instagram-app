8683ddfa3767e341ee0c617eefeef009713e889c
nelo <nelokwaz@gmail.com>
2021-04-25 08:24:09 +0100

    added the query to get more posts from user

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -160,4 +160,24 @@ export const EXPLORE_POSTS = gql`
   }
 `;
 
-export const GET_MORE_POSTS_FROM_USER = gql``;
+export const GET_MORE_POSTS_FROM_USER = gql`
+  query getMorePostsFromUser($userId: uuid!, $postId: uuid!) {
+    posts(
+      limit: 6
+      where: { user_id: { _eq: $userId }, _not: { id: { _eq: $postId } } }
+    ) {
+      id
+      media
+      likes_aggregate {
+        aggregate {
+          count
+        }
+      }
+      comments_aggregate {
+        aggregate {
+          count
+        }
+      }
+    }
+  }
+`;
