38dd5ccf66ff2bd3def3d616391fe3f55ce014ee
nelo <nelokwaz@gmail.com>
2021-07-30 06:01:48 +0100

    added the logic to handle add comment

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,12 +1,25 @@
+import { useMutation } from "@apollo/react-hooks";
 import { Button, TextField } from "@material-ui/core";
 import React, { useContext } from "react";
 import { UserContext } from "../../App";
+import { CREATE_COMMENT } from "../../graphql/mutations";
 import { useFeedPostStyles } from "../../styles";
 
 function Comment({ postId }) {
   const classes = useFeedPostStyles();
   const { feedIds, currentUserId } = useContext(UserContext);
   const [content, setContent] = React.useState("");
+  const [createComment] = useMutation(CREATE_COMMENT);
+
+  function handleAddComment() {
+    const variables = {
+      content,
+      postId,
+      userId: currentUserId,
+    };
+    createComment({ variables, update: handleUpdate });
+  }
+
   return (
     <div className={classes.commentContainer}>
       <TextField
@@ -26,6 +39,7 @@ function Comment({ postId }) {
         }}
       />
       <Button
+        onClick={handleAddComment}
         color="primary"
         className={classes.commentButton}
         disabled={!content.trim()}
