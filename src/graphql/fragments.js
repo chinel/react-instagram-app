a43f574672dac0e2c84d131615fb1aa5c5c576ed
nelo <nelokwaz@gmail.com>
2021-08-02 07:07:47 +0100

    created a reusable users field

--- src/graphql/fragments.js
+++ src/graphql/fragments.js
@@ -1,1 +1,11 @@
 import { gql } from "apollo-boost";
+
+//we are trying to create reusable fields for the users table
+export const userFields = gql`
+  fragment userFields on users {
+    id
+    name
+    username
+    profile_image
+  }
+`;
